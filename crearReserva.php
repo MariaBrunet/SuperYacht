a332502976fbe0606c14824da011d000cb8f7593
MariaBrunet <maria_20.6@hotmail.com>
2016-07-06 20:17:20 +0200

    Reservesgit add .

---
+++ crearReserva.php
@@ -0,0 +1,34 @@
+<?
+
+    //agafam dades de html
+    $idBarco = 1;
+    $nom = $_POST['nom'];
+    $llinatge1 = $_POST['llinatge1'];
+    $llinatge2 = $_POST['llinatge2'];
+    $dni = $_POST['dni'];
+    $telf = $_POST['telf'];
+    $direccio = $_POST['dir'];
+    $entrada = $_POST['entrada'];
+    $sortida = $_POST['salida'];
+
+    //carregam xml actual
+    $xml = simplexml_load_file('xml/reserves.xml');
+
+    //cream jerarquia amb dades
+    $reserva = $xml->addChild('reserva');
+    $reserva->addChild('idBarco', $idBarco);
+    $persona = $reserva->addChild('persona');
+    $persona->addChild('nom', $nom);
+    $persona->addChild('llinatge1', $llinatge1);
+    $persona->addChild('llinatge2', $llinatge2);
+    $persona->addChild('dni', $dni);
+    $persona->addChild('telf', $telf);
+    $persona->addChild('direccio', $direccio);
+    $data = $reserva->addChild('data');
+    $data->addChild('entrada', $entrada);
+    $data->addChild('sortida', $sortida);
+
+    //guardam nova reserva
+    $xml->asXML("xml/reserves.xml");
+
+?>
